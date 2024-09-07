ad5695d8a801310e471ce293fd78850e7e245756
Thomas Wright <thomaswright1@gmail.com>
2024-09-08 05:29:36 +0900

    explore img link fetching

--- scripts/getImageLinks.node.js
+++ scripts/getImageLinks.node.js
@@ -3,8 +3,25 @@ import * as cheerio from "cheerio";
 
 let typefaces = [
   {
+    name: "Comic Sans",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/ComicSansSpec3.svg/1733px-ComicSansSpec3.svg.png",
+    wikipediaPage: "https://en.wikipedia.org/wiki/Comic_Sans",
+    released: 1994,
+    classification: "Script",
+  },
+  {
+    name: "Curlz",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/4/46/CurlzSpec.svg",
+    wikipediaPage: "https://en.wikipedia.org/wiki/Curlz",
+    released: 1995,
+    classification: "Display",
+  },
+  {
     name: "Courier New",
-    sampleImg: "https://en.wikipedia.org/wiki/File:ConsolasSP.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/3/36/IBMCourier.svg",
     wikipediaPage:
       "https://en.wikipedia.org/wiki/Courier_(typeface)#Courier_New",
     released: 1995,
@@ -12,245 +29,276 @@ let typefaces = [
   },
   {
     name: "Consolas",
-    sampleImg: "https://en.wikipedia.org/wiki/File:ConsolasSP.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d5/ConsolasSP.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Consolas",
     released: 2007,
     classification: "Monospaced",
   },
   {
     name: "Gills Sans ",
-    sampleImg: "https://en.wikipedia.org/wiki/File:GillSansEG.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/4/48/GillSansEG.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Gill_Sans",
     released: 1928,
     classification: "Humanist",
   },
   {
     name: "Frutiger",
-    sampleImg: "https://en.wikipedia.org/wiki/File:FrutigerSpec-1.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e5/FrutigerSpec-1.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Frutiger_(typeface)",
     released: 1976,
     classification: "Humanist",
   },
   {
     name: "Optima",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Optima_font_sample.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/7/70/Optima_font_sample.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Optima",
     released: 1958,
     classification: "Humanist",
   },
   {
     name: "Verdana",
-    sampleImg: "https://en.wikipedia.org/wiki/File:VerdanaSpecimen.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/0/01/VerdanaSpecimen.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Verdana",
     released: 1996,
     classification: "Humanist",
   },
   {
     name: "Tahoma",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Tahoma.png",
+    sampleImg: "",
     wikipediaPage: "https://en.wikipedia.org/wiki/Tahoma_(typeface)",
     released: 1994,
     classification: "Humanist",
   },
   {
     name: "Droid Sans",
-    sampleImg: "https://en.wikipedia.org/wiki/File:DroidSerifSpecimen.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d4/DroidSerifSpecimen.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Droid_(typeface)",
     released: 2008,
     classification: "Humanist",
   },
   {
     name: "Calibri",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Calibri_sample_image.png",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b0/Calibri_sample_image.png",
     wikipediaPage: "https://en.wikipedia.org/wiki/Calibri",
     released: 2007,
     classification: "Humanist",
   },
   {
     name: "Segoe UI",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Segoe_UI_Example.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/7/72/Segoe_UI_Example.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Segoe#Segoe_UI",
     released: 2003,
     classification: "Humanist",
   },
   {
     name: "Akzidenz-Grotesk",
-    sampleImg: "https://en.wikipedia.org/wiki/File:AkzidenzGroteskspecAIB1.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e1/AkzidenzGroteskspecAIB1.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Akzidenz-Grotesk",
     released: 1898,
     classification: "Grotesque",
   },
   {
     name: "Helvetica",
-    sampleImg: "https://en.wikipedia.org/wiki/File:HelveticaSpecimenCH.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/2/28/HelveticaSpecimenCH.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Helvetica",
     released: 1957,
     classification: "Grotesque",
   },
   {
     name: "Univers",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Univers_sample_2015.png",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/3/30/Univers_sample_2015.png",
     wikipediaPage: "https://en.wikipedia.org/wiki/Univers",
     released: 1957,
     classification: "Grotesque",
   },
   {
     name: "Arial",
-    sampleImg: "https://en.wikipedia.org/wiki/File:ArialMTsp.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b1/ArialMTsp.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Arial",
     released: 1982,
     classification: "Grotesque",
   },
   {
     name: "Roboto",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Roboto_Font_Sample.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e7/Roboto_Font_Sample.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Roboto",
     released: 2011,
     classification: "Grotesque",
   },
   {
     name: "Franklin Gothic",
-    sampleImg: "https://en.wikipedia.org/wiki/File:FranklinGothicSP.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/3/35/FranklinGothicSP.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Franklin_Gothic",
     released: 1902,
     classification: "Geometric",
   },
   {
     name: "Futura",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Futura_Specimen.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/5/50/Futura_Specimen.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Futura_(typeface)",
     released: 1927,
     classification: "Geometric",
   },
   {
     name: "Impact",
-    sampleImg: "https://en.wikipedia.org/wiki/File:ImpactSP.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/2/2b/ImpactSP.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Impact_(typeface)",
     released: 1965,
-    classification: "Geometric",
+    classification: "Grotesque",
   },
   {
     name: "Metro",
-    sampleImg: "https://en.wikipedia.org/wiki/File:MetroSpecimen.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/8/87/MetroSpecimen.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Metro_(typeface)",
     released: 1929,
     classification: "Geometric",
   },
   {
     name: "FF DIN",
-    sampleImg: "https://en.wikipedia.org/wiki/File:FFDIN.svg",
+    sampleImg: "https://upload.wikimedia.org/wikipedia/commons/c/cd/FFDIN.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/FF_DIN",
     released: 1995,
     classification: "Geometric",
   },
   {
     name: "Century Gothic",
-    sampleImg: "https://en.wikipedia.org/wiki/File:CenturyGothicSpecimen.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/4/4f/CenturyGothicSpecimen.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Century_Gothic",
     released: 1990,
     classification: "Geometric",
   },
   {
     name: "Avenir",
-    sampleImg: "https://en.wikipedia.org/wiki/File:AvenirSP.png",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/0/07/AvenirSP.png",
     wikipediaPage: "https://en.wikipedia.org/wiki/Avenir_(typeface)",
     released: 1988,
     classification: "Geometric",
   },
   {
     name: "Gotham",
-    sampleImg: "https://en.wikipedia.org/wiki/File:GothamSpec.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/4/4e/GothamSpec.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Gotham_(typeface)",
     released: 2000,
     classification: "Geometric",
   },
   {
     name: "Didot",
-    sampleImg: "https://en.wikipedia.org/wiki/File:DidotSP.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/f/f9/DidotSP.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Didot_(typeface)",
     released: 1800,
     classification: "Didone",
   },
   {
     name: "Bodoni",
-    sampleImg: "https://en.wikipedia.org/wiki/File:ITCBodoni.png",
+    sampleImg: "",
     wikipediaPage: "https://en.wikipedia.org/wiki/Bodoni",
     released: 1795,
     classification: "Didone",
   },
   {
     name: "Century Schoolbook",
-    sampleImg: "https://en.wikipedia.org/wiki/File:CenturySchoolbookSAIB.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/8/8b/CenturySchoolbookSAIB.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Century_type_family",
     released: 1918,
     classification: "Didone",
   },
   {
     name: "Garamond",
-    sampleImg: "https://en.wikipedia.org/wiki/File:GaramondSpecimenA.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/6/60/GaramondSpecimenA.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Garamond",
     released: 1495,
     classification: "OldStyle",
   },
   {
     name: "Baskerville",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Baskerville_font_sample.png",
+    sampleImg: "",
     wikipediaPage: "https://en.wikipedia.org/wiki/Baskerville",
     released: 1755,
     classification: "Transitional",
   },
   {
     name: "Caslon",
-    sampleImg: "https://en.wikipedia.org/wiki/File:CaslonSp.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e5/CaslonSp.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Caslon",
     released: 1725,
     classification: "OldStyle",
   },
   {
     name: "Bembo",
-    sampleImg: "https://en.wikipedia.org/wiki/File:ET_Book_sample.png",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/3/38/ET_Book_sample.png",
     wikipediaPage: "https://en.wikipedia.org/wiki/Bembo",
     released: 1496,
     classification: "OldStyle",
   },
   {
     name: "Times New Roman",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Times_New_Roman-sample.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/2/21/Times_New_Roman-sample.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Times_New_Roman",
     released: 1932,
     classification: "Transitional",
   },
   {
     name: "Sabon",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Sabon_lt_std_preview.png",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/2/2c/Sabon_lt_std_preview.png",
     wikipediaPage: "https://en.wikipedia.org/wiki/Sabon",
     released: 1967,
     classification: "Transitional",
   },
   {
     name: "Palatino",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Palatino_font_sample.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/1/1a/Palatino_font_sample.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Palatino",
     released: 1949,
     classification: "Transitional",
   },
   {
     name: "Georgia",
-    sampleImg: "https://en.wikipedia.org/wiki/File:GeorgiaSpecimenAIB.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/b/ba/GeorgiaSpecimenAIB.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Georgia_(typeface)",
     released: 1996,
     classification: "Transitional",
   },
   {
     name: "Minion",
-    sampleImg: "https://en.wikipedia.org/wiki/File:Minion_opticals_sample.png",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/9/90/Minion_opticals_sample.png",
     wikipediaPage: "https://en.wikipedia.org/wiki/Minion_(typeface)",
     released: 1990,
     classification: "Transitional",
   },
   {
     name: "Cambria",
-    sampleImg: "https://en.wikipedia.org/wiki/File:CambriaSpecimen.svg",
+    sampleImg:
+      "https://upload.wikimedia.org/wikipedia/commons/b/bd/CambriaSpecimen.svg",
     wikipediaPage: "https://en.wikipedia.org/wiki/Cambria_(typeface)",
     released: 2005,
     classification: "Transitional",
@@ -277,6 +325,21 @@ async function fetchOriginalFileLink(url) {
   }
 }
 
+async function sampleImg(url) {
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    // Find the anchor tag with the text "Original File"
+    const link = $("img").eq(3).attr("src");
+
+    return link ? "https:" + link : `No "Original File" link found on ${url}`;
+  } catch (error) {
+    return `Error fetching ${url}: ${error.message}`;
+  }
+}
+
 // Main function to process an array of URLs
 // async function fetchAllOriginalFileLinks(urls) {
 //   const results = await Promise.all(urls.map(fetchOriginalFileLink));
@@ -286,10 +349,10 @@ async function fetchOriginalFileLink(url) {
 async function main() {
   let modified = await Promise.all(
     typefaces.map(async (typeface) => {
-      let result = await fetchOriginalFileLink(typeface.sampleImg);
+      let result = await sampleImg(typeface.wikipediaPage);
       return {
         ...typeface,
-        sampleImg: result,
+        imageLink: result,
       };
     })
   );
