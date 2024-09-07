6c092822eff027157b3ce5b86e7adaa00de91918
Thomas Wright <thomaswright1@gmail.com>
2024-09-07 18:56:58 +0900

    add homepage

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://thomaswright.github.io/typeline/",
   plugins: [
     react({
       include: ["**/*.res.mjs"],
